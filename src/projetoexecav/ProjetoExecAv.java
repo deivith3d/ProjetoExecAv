aeb34ed9e5c93ec20980ee43b53d8833c005003c
deivith3d <deivith3d@gmail.com>
2023-11-29 11:46:32 -0300

    primeiro

---
+++ src/projetoexecav/ProjetoExecAv.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetoexecav;
+
+/**
+ *
+ * @author deivi
+ */
+public class ProjetoExecAv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
